7fbc105df71d0e34fc40097083c82e97d7c135bf
Armands <armands.bidzans@gmail.com>
2025-02-09 21:50:42 +0200

    a little fix, added colors for few text moments and system cls

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
 #include "data.h"
+#include <cstdlib>
 
 using namespace std;
+
+#define COLOR_RESET      "\033[0m"
+#define COLOR_RED        "\033[31m"
+#define COLOR_GREEN      "\033[32m"
+#define COLOR_DARK_GREEN "\033[2;32m"  // Темно-зеленый
+#define COLOR_CYAN       "\033[36m"
+#define COLOR_BLUE       "\033[34m"
+#define COLOR_GRAY       "\033[90m"
+#define COLOR_YELLOW     "\033[33m"
+
+void clearScreen() {
+    system("cls||clear");
+}
 
 int main() {
     loadData();
     int choice;
     do {
-        cout << "\nCar Rental System Menu:\n"
-            << "1. Add car\n"
-            << "2. Add client\n"
-            << "3. Show cars\n"
-            << "4. Show clients\n"
-            << "5. Search (car/client)\n"
-            << "6. Delete (entry/all data)\n"
-            << "7. Calculate client debt\n"
-            << "8. Exit\n"
+        clearScreen();
+        cout << COLOR_YELLOW "\nCar Rental System Menu:\n" COLOR_RESET
+            << COLOR_GREEN "1. Add car\n"
+            << "2. Add client\n" COLOR_RESET
+            << COLOR_DARK_GREEN "3. Show cars\n"  // Темно-зеленый
+            << "4. Show clients\n" COLOR_RESET    // Темно-зеленый
+            << COLOR_CYAN "5. Search (car/client)\n" COLOR_RESET
+            << COLOR_RED "6. Delete (entry/all data)\n" COLOR_RESET
+            << COLOR_BLUE "7. Calculate client debt\n" COLOR_RESET
+            << COLOR_GRAY "8. Exit\n" COLOR_RESET
             << "Enter your choice: ";
         cin >> choice;
+        clearScreen();
+
         switch (choice) {
         case 1:
             addCar();
@@ -33,14 +50,16 @@ int main() {
             break;
         case 5: {
             int sub;
-            cout << "Search: 1.Car 2.Client\nChoice: ";
+            cout << COLOR_CYAN "Search: 1.Car 2.Client\nChoice: " COLOR_RESET;
             cin >> sub;
+            clearScreen();
             if (sub == 1)
                 searchCar();
             else if (sub == 2)
                 searchClient();
-            else
-                cout << "Invalid choice.\n";
+            else {
+                cout << COLOR_RED "Invalid choice.\n" COLOR_RESET;
+            }
             break;
         }
         case 6:
@@ -50,12 +69,17 @@ int main() {
             calcDebt();
             break;
         case 8:
-            cout << "Exiting and saving data...\n";
+            cout << COLOR_GRAY "Exiting and saving data...\n" COLOR_RESET;
             saveData();
             break;
         default:
-            cout << "Invalid choice. Please enter 1-8.\n";
-            exit(EXIT_FAILURE);
+            cout << COLOR_RED "Invalid choice. Please enter 1-8.\n" COLOR_RESET;
+            break;
+        }
+        if (choice != 8) {
+            cout << "\nPress Enter to continue...";
+            cin.ignore();
+            cin.get();
         }
     } while (choice != 8);
     return 0;
